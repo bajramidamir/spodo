592f71b7499effc634076b0cb79d59ed3a0abf02
Damir Bajrami <bajrami.damir@gmail.com>
2023-05-11 19:11:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='text-center my-12'>
+        <p className='text-5xl mb-4'>SpoDo</p>
+        <p className='text-lg'>Spotify album / playlist downloader</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
